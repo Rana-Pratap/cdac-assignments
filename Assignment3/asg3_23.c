bec0a0c37a8397652876447957d786090e1136d4
Rana-Pratap <ranapratap7777777@gmail.com>
2023-03-27 00:14:22 +0530

    added few changes

---
+++ Assignment3/asg3_23.c
@@ -0,0 +1,32 @@
+/*
+       0 1 3 4 5 6 7
+i=0    * * * 1 * * *
+i=1    * * 1 * 1 * *
+i=2    * 1 * 2 * 1 *
+i=3    1 * 3 * 3 * 1
+
+*/
+
+
+
+#include <stdio.h>
+int main() {
+   int rows, coef = 1, space, i, j;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 0; i < rows; i++) {
+      for (space = 1; space <= rows - i; space++)
+         printf("  ");
+      for (j = 0; j <= i; j++) {
+         if (j == 0 || i == 0)
+            coef = 1;
+         else
+            coef = coef * (i - j + 1) / j;
+         printf("%4d", coef);
+      }
+      printf("\n");
+   }
+
+
+    return 0;
+}
